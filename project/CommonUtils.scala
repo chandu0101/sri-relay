45787eac6beb83d93038132523d49fb7b7b040e6
Dan Di Spaltro <dan.dispaltro@gmail.com>
2016-12-14 10:18:13 -0800

    Add cross building for 2.12

--- project/CommonUtils.scala
+++ project/CommonUtils.scala
@@ -4,8 +4,6 @@ import sbt._
 
 object CommonUtils {
 
-
-
   lazy val commonSettings =
     Seq(
       organization := "com.github.chandu0101",
@@ -13,10 +11,10 @@ object CommonUtils {
       homepage := Some(url("https://github.com/chandu0101/sri-relay")),
       licenses +=("Apache-2.0", url("http://opensource.org/licenses/Apache-2.0")),
       scalaVersion := Dependencies.Version.scala211,
+      crossScalaVersions := Seq(Dependencies.Version.scala211, Dependencies.Version.scala212),
       scalacOptions ++= Seq("-deprecation", "-unchecked", "-feature",
         "-language:postfixOps", "-language:implicitConversions",
         "-language:higherKinds", "-language:existentials"))
-
 
 
   def DefProject(dir: String, _id: String = "") = {
